e452796f521245eb511352b6e61ec9ca64522f38
Syarif Miftahudin <miftahudin.syarif@gmail.com>
2025-04-18 14:59:34 +0700

    feat: tambahkan dukungan untuk analitik dengan mengintegrasikan @vercel/analytics ke dalam aplikasi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./component/Navbar";
 import Footer from "./component/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col`}
       >
+        <Analytics />
         <Navbar />
         <div>{children}</div>
         <Footer />
